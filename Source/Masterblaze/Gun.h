bbf239f2beab2ad6d11d39228032d1e889635660
Barniatya <girmany321@gmail.com>
2020-05-05 05:59:44 +0200

    MB2.2 Refactor

--- Source/Masterblaze/Gun.h
+++ Source/Masterblaze/Gun.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Masterblaze/MasterblazeProjectile.h"
 #include "Gun.generated.h"
 
 UCLASS()
@@ -11,16 +12,39 @@ class MASTERBLAZE_API AGun : public AActor
 {
 	GENERATED_BODY()
 	
+	/** Gun mesh: 1st person view (seen only by self) */
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+	class USkeletalMeshComponent* FP_Gun;
+
+	/** Location on gun mesh where projectiles should spawn. */
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+		class USceneComponent* FP_MuzzleLocation;
 public:	
 	// Sets default values for this actor's properties
 	AGun();
 
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	/** Fires a projectile. */
+	void OnFire();
+
+	/** Projectile class to spawn */
+	UPROPERTY(EditDefaultsOnly, Category = Projectile)
+	TSubclassOf<class AMasterblazeProjectile> ProjectileClass;
+
+	UPROPERTY(EditDefaultsOnly, Category = Projectile)
+	class UAnimInstance* AnimInstance;
+
+	/** Sound to play each time we fire */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	class USoundBase* FireSound;
+
+	/** AnimMontage to play each time we fire */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	class UAnimMontage* FireAnimation;
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
 
 };
