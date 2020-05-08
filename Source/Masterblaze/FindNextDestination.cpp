8dd4320b60045d629af7790b9704913e27740ede
Barniatya <girmany321@gmail.com>
2020-05-08 12:13:29 +0200

    MB11 Added classes for GuardBehaviour

---
+++ Source/Masterblaze/FindNextDestination.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FindNextDestination.h"
+
+EBTNodeResult::Type UFindNextDestination::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) 
+{
+    return EBTNodeResult::Succeeded;
+ }
